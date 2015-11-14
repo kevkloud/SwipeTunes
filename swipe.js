36cefa1d863868506e6475bdabb5d0e62296e17c
Kevin Kloud <kevin.hj.lee@gmail.com>
2015-11-14 16:43:25 -0500

    Search form

--- swipe.js
+++ swipe.js
@@ -1,8 +1,16 @@
 
 var client = new WindowsAzure.MobileServiceClient(
-    "https://swipetunesserver.azure-mobile.net/",
-    "KAfLYWfIFFndZengKOGsHiQsNxIKXL27"
+  "https://swipetunesserver.azure-mobile.net/",
+  "KAfLYWfIFFndZengKOGsHiQsNxIKXL27"
 );
 
-var item = { text: "Awesome item" };
-client.getTable("Item").insert(item);
+var search_services() {
+  
+  var search_query = document.getElementById("search_form").value;
+  
+  alert("You searched for: " + search_query);
+  
+}
+
+// var item = { text: "Awesome item" };
+// client.getTable("Item").insert(item);
