d41ff7968f77764276b9b8e229f8e7617f7e0cfc
Kevin Kloud <kevin.hj.lee@gmail.com>
2015-11-14 23:57:26 -0500

    Properly fetching data from Spotify endpoints

--- static/swipe.js
+++ static/swipe.js
@@ -4,89 +4,22 @@ var client = new WindowsAzure.MobileServiceClient(
   "KAfLYWfIFFndZengKOGsHiQsNxIKXL27"
 );
 
-// find template and compile it
-var templateSource = document.getElementById('results-template').innerHTML,
-    template = Handlebars.compile(templateSource),
-    resultsPlaceholder = document.getElementById('results'),
-    playingCssClass = 'playing',
-    audioObject = null;
-
-var fetchTracks = function (albumId, callback) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/albums/' + albumId,
-        success: function (response) {
-            callback(response);
-        }
-    });
-};
-
-var searchAlbums = function (query) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        data: {
-            q: query,
-            type: 'album'
-        },
-        success: function (response) {
-            resultsPlaceholder.innerHTML = template(response);
-        }
-    });
-};
-
-results.addEventListener('click', function (e) {
-    var target = e.target;
-    if (target !== null && target.classList.contains('cover')) {
-        if (target.classList.contains(playingCssClass)) {
-            audioObject.pause();
-        } else {
-            if (audioObject) {
-                audioObject.pause();
-            }
-            fetchTracks(target.getAttribute('data-album-id'), function (data) {
-                audioObject = new Audio(data.tracks.items[0].preview_url);
-                audioObject.play();
-                target.classList.add(playingCssClass);
-                audioObject.addEventListener('ended', function () {
-                    target.classList.remove(playingCssClass);
-                });
-                audioObject.addEventListener('pause', function () {
-                    target.classList.remove(playingCssClass);
-                });
-            });
-        }
-    }
-});
-
-document.getElementById('search-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    searchAlbums(document.getElementById('query').value);
-}, false);
-
-/*
 // Called when a user searches an artist
 function search_services() {
   
   // Get the value currently in the search form
-  var query = document.getElementById("search_form").value;
+  var query = document.getElementById("query").value;
 
   // Replace spaces with %20 for URI search
-  //var encoded_query = encodeURIComponent(query.trim());
+  var encoded_query = encodeURIComponent(query.trim());
 
-  //var url = 'https://api.spotify.com/v1/search?type=track&limit=50&q=' + encoded_query;
+  var url = 'https://api.spotify.com/v1/search?type=track&limit=50&q=' + encoded_query;
 
-  $.ajax({
-    url: 'https://api.spotify.com/v1/search',
-    data: {
-      q: query,
-      type: 'track'
-    },
-    success: function (response) {
-      resultsPlaceholder.innerHTML = templlate(response);
-    }
+  var promise1 = fetch(url).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    var tracks = json.tracks;
+    var items = tracks.items;
+    return items;
   });
- 
 }
-*/
-
-// var item = { text: "Awesome item" };
-// client.getTable("Item").insert(item);
