f20ce27d5cdb3c4e4d34e21a9a08ea558ccee9d5
Kevin Kloud <kevin.hj.lee@gmail.com>
2015-11-14 20:36:39 -0500

    Adding search function for Spotify

--- static/swipe.js
+++ static/swipe.js
@@ -7,9 +7,49 @@ var client = new WindowsAzure.MobileServiceClient(
 // Called when a user searches an artist
 function search_services() {
   
-  var search_query = document.getElementById("search_form").value;
+  // Get the value currently in the search form
+  var query = document.getElementById("search_form").value;
+
+  // Replace spaces with %20 for URI search
+  var encoded_query = encodeURIComponent(query.trim());
+
+  var url = 'https://api.spotify.com/v1/search?type=track&limit=50&q=' + encoded_query;
   
-  alert("You searched for: " + search_query);
+  $.ajax(url, {
+    dataType: 'json',
+    success: function(r) {
+      console.log('got track', r);
+      callback({
+        word: word,
+        tracks: r.tracks.items
+          .map(function(item) {
+            var ret = {
+              name: item.name,
+              artist: 'Unknown',
+              artist_uri: '',
+              album: item.album.name,
+              album_uri: item.album.uri,
+              cover_url: '',
+              uri: item.uri
+            }
+            if (item.artists.length > 0) {
+              ret.artist = item.artists[0].name;
+              ret.artist_uri = item.artists[0].uri;
+            }
+            if (item.album.images.length > 0) {
+              ret.cover_url = item.album.images[item.album.images.length - 1].url;
+            }
+            return ret;
+          })
+      });
+    },
+    error: function(r) {
+      callback({
+        word: word,
+        tracks: []
+      });
+    }
+  }); 
   
 }
 
